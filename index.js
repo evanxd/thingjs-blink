d22be2f97ddd2a392ae01e5f48f0b225c5881f30
Evan Tseng <itoyxd@gmail.com>
2016-07-31 10:31:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * The run method is the start point of the extension.
    * @param {Object} params          The extension parameters.
-   * @param {Led}    params.modules      The LED instance.
+   * @param {Led}    params.modules  The LED instance.
    * @param {Number} params.interval The blinking rate.
    */
   run: function(params) {
